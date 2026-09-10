ac75536c9d5e22d444dfef7c238d508b11639f31
Jordan Vera <verawebdevelopment@gmail.com>
2026-09-10 05:46:09 -0500

    Comment out HomeAbout component in HomePage for future adjustments.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import { HomeAbout } from "@/components/home-about";
-import { HomeHero } from "@/components/home-hero";
-import { HomeServices } from "@/components/home-services";
-import { ContactCta } from "@/components/contact-cta";
-import { LogoMarquee } from "@/components/logo-marquee";
-import { WorkCarousel } from "@/components/work-carousel";
+import { HomeAbout } from '@/components/home-about';
+import { HomeHero } from '@/components/home-hero';
+import { HomeServices } from '@/components/home-services';
+import { ContactCta } from '@/components/contact-cta';
+import { LogoMarquee } from '@/components/logo-marquee';
+import { WorkCarousel } from '@/components/work-carousel';
 
 export default function HomePage() {
   return (
     <main>
       <HomeHero />
-      <HomeAbout />
+      {/* <HomeAbout /> */}
       <LogoMarquee />
       <HomeServices />
       <WorkCarousel />
