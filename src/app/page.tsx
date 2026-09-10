06e918f21564f83d689f22f49e732484a57b70e8
Jordan Vera <verawebdevelopment@gmail.com>
2026-09-10 05:46:20 -0500

    Uncomment HomeAbout component in HomePage to enable its display.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
   return (
     <main>
       <HomeHero />
-      {/* <HomeAbout /> */}
+      <HomeAbout />
       <LogoMarquee />
       <HomeServices />
       <WorkCarousel />
